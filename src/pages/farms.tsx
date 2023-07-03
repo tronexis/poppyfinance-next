6a892b0d1381957ba2bae13635e70d36a587e2d9
tronexis <tronexis>
2023-07-03 16:37:48 +0600

    feat: add live pricing slider

--- src/pages/farms.tsx
+++ src/pages/farms.tsx
@@ -1,15 +1,20 @@
+import Farms from '@/components/farms/farms';
+import { LAYOUT_OPTIONS } from '@/lib/constants';
+import LivePricingSlider from '@/components/ui/live-pricing-slider';
 import type { NextPageWithLayout } from '@/types';
 import { NextSeo } from 'next-seo';
 import RootLayout from '@/layouts/_root-layout';
-import Farms from '@/components/farms/farms';
+import { useLayout } from '@/lib/hooks/use-layout';
 
 const FarmsPage: NextPageWithLayout = () => {
+  const { layout } = useLayout();
   return (
     <>
       <NextSeo
         title="Farms"
         description="Criptic - React Next Web3 NFT Crypto Dashboard Template"
       />
+      {layout === LAYOUT_OPTIONS.POPPY && <LivePricingSlider limits={4} />}
       <Farms />
     </>
   );
